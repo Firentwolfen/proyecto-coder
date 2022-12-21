39f6a316a15e31934bb966d622d53a84752d242b
Manuel L <manuel.loyola.cgbr@gmail.com>
2022-12-21 19:13:13 -0300

    Primer subida
    Primer entrega de React para CoderHouse

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ItemListContainer from './components/ItemListContainer';
+//import Dogs from "./pages/Dogs"
+//import Cats from "./pages/Cats"
+//import Sheeps from "./pages/Sheeps"
+//import Goats from "./pages/Goats"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Router>
+      <Navbar />
+      <ItemListContainer gretting="Bienvenido a PokeMartFan"/>
+      {/*<Switch>
+        <Route path='/' exact component={Dogs} />
+        <Route path='/Productos' component={Cats} />
+        <Route path='/Contacto' component={Sheeps} />
+        <Route path='/RedesSociales' component={Goats} />
+  </Switch>*/}
+    </Router>
     </div>
+  
   );
 }
 
